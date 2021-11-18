7ee3e94f6124a6071e69b9399f1e87c4aab580ee
melacsirrah575 <melacsirrah575@gmail.com>
2021-11-18 16:04:21 -0400

    Ammo pickups increase player's ammo

--- Assets/Scripts/AmmoPickup.cs
+++ Assets/Scripts/AmmoPickup.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class AmmoPickup : MonoBehaviour
 {
+    [SerializeField] int ammoAmount = 5;
+    [SerializeField] AmmoType ammoType;
+
     private void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.tag == "Player")
         {
+            FindObjectOfType<Ammo>().IncreaseCurrentAmmo(ammoType, ammoAmount);
             Debug.Log("You grabbed ammo!");
             Destroy(gameObject);
         }
