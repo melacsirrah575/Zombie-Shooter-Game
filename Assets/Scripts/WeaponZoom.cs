d10d07fb9be86701677f2a82c0812d4c2e930436
melacsirrah575 <melacsirrah575@gmail.com>
2021-11-18 09:23:11 -0400

    Update WeaponZoom.cs
    Aim sensitivity decreased when ADS

--- Assets/Scripts/WeaponZoom.cs
+++ Assets/Scripts/WeaponZoom.cs
@@ -1,14 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
 
 public class WeaponZoom : MonoBehaviour
 {
     [SerializeField] Camera fpsCamera;
     [SerializeField] float zoomedOutFOV = 60f;
-    [SerializeField] [Range(1f, 60f)] float zoomedInFOV;
+    [SerializeField] [Range(1f, 60f)] float zoomedInFOV = 20;
+    [SerializeField] float zoomedOutSensitivity = 2f;
+    [SerializeField] [Range(0.1f, 2f)] float zoomedInSensitivity = .5f;
+
+
+    RigidbodyFirstPersonController fpsController;
 
     bool zoomedInToggle = false;
+
+    private void Start()
+    {
+        fpsController = GetComponent<RigidbodyFirstPersonController>();
+    }
 
     void Update()
     {
@@ -18,10 +29,15 @@ public class WeaponZoom : MonoBehaviour
             {
                 zoomedInToggle = true;
                 fpsCamera.fieldOfView = zoomedInFOV;
-            } else
+                fpsController.mouseLook.XSensitivity = zoomedInSensitivity;
+                fpsController.mouseLook.YSensitivity = zoomedInSensitivity;
+            }
+            else
             {
                 zoomedInToggle = false;
                 fpsCamera.fieldOfView = zoomedOutFOV;
+                fpsController.mouseLook.XSensitivity = zoomedOutSensitivity;
+                fpsController.mouseLook.YSensitivity = zoomedOutSensitivity;
             }
         }
     }
