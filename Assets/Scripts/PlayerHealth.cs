c934aa39a9b08cdd31e0e9f596b8dc8f22013540
melacsirrah575 <melacsirrah575@gmail.com>
2021-11-22 19:04:59 -0400

    Putting box colliders on assets that didn't have them

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -1,25 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+using TMPro;
 
 public class PlayerHealth : MonoBehaviour
 {
     [SerializeField] float maxHealth = 100f;
     [SerializeField] AudioClip playerHurt;
+    [SerializeField] TextMeshProUGUI playerHealth;
 
     float currentHealth;
 
     void Start()
     {
         currentHealth = maxHealth;
+        playerHealth.text = maxHealth.ToString();
     }
 
     public void PlayerTakeDamage(float damage)
     {
         AudioSource.PlayClipAtPoint(playerHurt, transform.position);
+
         currentHealth -= damage;
+        playerHealth.text = currentHealth.ToString();
+
         if(currentHealth <= 0)
         {
+            playerHealth.text = "0";
             GetComponent<DeathHandler>().HandleDeath();
         }
     }
